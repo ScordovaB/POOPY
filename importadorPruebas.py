90b94f06d2d59fc701f17426bc80babcc4c04ea9
Santiago Cordova <di730657@iteso.mx>
2022-08-01 14:45:03 -0500

    Nueva Clase Payment

--- importadorPruebas.py
+++ importadorPruebas.py
@@ -1,9 +1,13 @@
 from prueba import Car
+from prueba import Payment
 
 car_1= Car("BMW","Serie 3","2022","Blanco")
+pago = Payment("Credit")
 
 print(car_1.make)
 print(car_1.model)
 print(car_1.color)
 car_1.drive()
 car_1.stop()
+
+pago.decifrar()
--- prueba.py
+++ prueba.py
@@ -9,3 +9,11 @@ class Car:
         print("En circulacion el "+ self.make +" "+self.model+ " Del año "+self.year +" y es del color "+ self.color)
     def stop(self):
         print("Carro " +self.make +" "+ self.model+ " se detuvo.")
+
+class Payment:
+    def __init__(self,type):
+        self.type = type
+
+    def decifrar(self):
+        if(self.type == 'Credit'):
+            print("Paying with Credit Card")
